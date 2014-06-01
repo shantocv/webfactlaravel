51e5aadeed215b59ca71decaf7e6fc1256aae901
shanto <shantocv@gmail.com>
2014-06-01 23:14:20 +0530

    "first push to github all laravel files"

---
+++ app/database/migrations/2014_05_30_134251_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users',function($table){
+			$table->increments('user_id');
+			$table->string('name',50);
+			$table->string('email');
+			$table->string('password');
+			$table->string('verify');
+			$table->boolean('status')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
